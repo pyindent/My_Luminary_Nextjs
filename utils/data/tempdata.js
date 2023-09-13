a4bf3168b46f2c9ccd635222ab0f30e737697492
pyindent <randall.thomas.sun@gmail.com>
2023-09-13 10:28:59 -0400

    update best sellers

---
+++ utils/data/tempdata.js
@@ -0,0 +1,178 @@
+export const bestSellingProducts = [
+    {
+        name: "Biscotti (Indica) - 3.5g",
+        categories: [
+            {
+                name : "CRAFT FLOWER",
+                slug : "craft-flower",
+                __typename: "ProductCategory"
+            }
+        ],
+        discount: 0,
+        is_featured: null,
+        is_new: null,
+        pictures: [
+            {
+                url: "/images/products/product1.jpg",
+                height: 300,
+                width: 300,
+                __typename: "Media"
+            }
+        ],
+        price: [40, 60],
+        ratings: 4.5,
+        reviews: 2,
+        short_description: "Help with Anxiety, Pain, Depression, Stress",
+        slug: "biscotti-indica-3.5",
+        small_pictures: null,
+        variants: [
+            {
+                price: 60,
+                sale_price: null,
+                __typename: "Variant"
+            },
+            {
+                price: 40,
+                sale_price: null,
+                __typename: "Variant"
+            },
+            {
+                price: 50,
+                sale_price: null,
+                __typename: "Variant"
+            },
+        ]
+    },
+    {
+        name: "Optimal Gummies 100mg",
+        categories: [
+            {
+                name : "EDIBLES",
+                slug : "edibles",
+                __typename: "ProductCategory"
+            }
+        ],
+        discount: 0,
+        is_featured: true,
+        is_new: true,
+        pictures: [
+            {
+                url: "/images/products/product2.jpg",
+                height: 300,
+                width: 300,
+                __typename: "Media"
+            }
+        ],
+        price: [20, 25],
+        ratings: 5,
+        reviews: 4,
+        short_description: "Indulge in the delicious and effective Optimal Gummies. Infused with premium THC, each gummy offers a delightful burst of flavor while delivering the potential benefits of THC. Experience relaxation, calmness, and a sense of overall well-being with these tasty treats. Enjoy the optimal way to incorporate THC into your daily routine",
+        slug: "optimal-gummies-100",
+        small_pictures: null,
+        variants: [
+            {
+                price: 25,
+                sale_price: null,
+                __typename: "Variant"
+            },
+            {
+                price: 22,
+                sale_price: null,
+                __typename: "Variant"
+            },
+            {
+                price: 20,
+                sale_price: null,
+                __typename: "Variant"
+            },
+        ]
+    },
+    {
+        name: "Steel Magnolia Capsules",
+        categories: [
+            {
+                name : "MUSHROOMS",
+                slug : "mushrooms",
+                __typename: "ProductCategory"
+            }
+        ],
+        discount: 0,
+        is_featured: null,
+        is_new: null,
+        pictures: [
+            {
+                url: "/images/products/product3.jpg",
+                height: 300,
+                width: 300,
+                __typename: "Media"
+            }
+        ],
+        price: [40, 60],
+        ratings: 3.5,
+        reviews: 2,
+        short_description: "Potency: Medium to High in potency",
+        slug: "steel-magnolia-capsules",
+        small_pictures: null,
+        variants: [
+            {
+                price: 60,
+                sale_price: null,
+                __typename: "Variant"
+            },
+            {
+                price: 40,
+                sale_price: null,
+                __typename: "Variant"
+            },
+            {
+                price: 50,
+                sale_price: null,
+                __typename: "Variant"
+            },
+        ]
+    },
+    {
+        name: "Sharpstone Grinders",
+        categories: [
+            {
+                name : "SMOKE ACCESSORIES",
+                slug : "smoke-accessories",
+                __typename: "ProductCategory"
+            }
+        ],
+        discount: 0,
+        is_featured: null,
+        is_new: true,
+        pictures: [
+            {
+                url: "/images/products/product4.jpg",
+                height: 300,
+                width: 300,
+                __typename: "Media"
+            }
+        ],
+        price: [25, 35],
+        ratings: 4.5,
+        reviews: 2,
+        short_description: "Introducing Sharpstone Grinders: The Epitome of Precision and Efficiency",
+        slug: "sharpstone-grinders",
+        small_pictures: null,
+        variants: [
+            {
+                price: 35,
+                sale_price: null,
+                __typename: "Variant"
+            },
+            {
+                price: 25,
+                sale_price: null,
+                __typename: "Variant"
+            },
+            {
+                price: 20,
+                sale_price: null,
+                __typename: "Variant"
+            },
+        ]
+    }
+]
