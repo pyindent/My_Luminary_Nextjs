f275dff55f8f21f77c38ef82515779b2ef91228c
pyindent <randall.thomas.sun@gmail.com>
2023-10-04 10:20:15 -0500

    update with enum

---
+++ server/schemas/enums/index.js
@@ -0,0 +1,13 @@
+// Enum for order_status
+export const OrderStatus = {
+    PAYMENT_PENDING: "payment-pending",
+    PAYMENT_PROCESSING: "payment-processing",
+    PAYMENT_SUCCESS: "payment-success",
+    PAYMENT_FAILED: "payment-failed"
+};
+  
+// Enum for payment_method
+export const PaymentMethod = {
+    CASH_ON_DELIVERY: "cash-on-delivery",
+    BANK_TRANSFER: "bank-transfer"
+};
---
+++ server/schemas/models/Order.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const status = require('../enums');
+const ORDER_STATUS = status.OrderStatus;
+const PAYMENT_METHOD = status.PaymentMethod;
+
+const Order = mongoose.models.Order || mongoose.model('Order', new mongoose.Schema({
+  placed_on: { 
+    type: String,  
+  },
+  actual_delivery_date: { 
+    type: String,  
+  },
+  paid_amount: {
+    type: Number
+  },
+  saved_amount: {
+    type: Number
+  },
+  customer_id: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'user'
+  },
+  order_status: {
+    type: String,
+    enum: [
+        ORDER_STATUS.PAYMENT_PENDING, 
+        ORDER_STATUS.PAYMENT_PROCESSING, 
+        ORDER_STATUS.PAYMENT_SUCCESS, 
+        ORDER_STATUS.PAYMENT_FAILED
+    ],
+    default: ORDER_STATUS.PAYMENT_PENDING,
+  },
+  payment_method: {
+    type: String,
+    enum: [
+        PAYMENT_METHOD.CASH_ON_DELIVERY, 
+        PAYMENT_METHOD.BANK_TRANSFER
+    ],
+    default: PAYMENT_METHOD.CASH_ON_DELIVERY,
+  }
+}, {timestamps: true}));
+
+module.exports = Order;
